6c6588f67c982334d975357d8090643cbe1ae5ce
Niall Buckley <buckleyniall100@gmail.com>
2023-08-30 15:17:28 -0500

    Add firebase function which updates database triggered by user creation

--- site-api-service/functions/src/index.ts
+++ site-api-service/functions/src/index.ts
@@ -1,10 +1,18 @@
-import {onRequest} from "firebase-functions/v2/https";
+import {initializeApp} from "firebase-admin/app";
+import {Firestore} from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
+import * as functions from "firebase-functions";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+initializeApp();
 
- export const helloWorld = onRequest((request, response) => {
-   logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
+const firestore = new Firestore();
+
+export const createUser = functions.auth.user().onCreate((user) => {
+    const userInfo = {
+        uid: user.uid,
+        email: user.email,
+        photoUrl: user.photoURL,
+    };
+    firestore.collection("users").doc(user.uid).set(userInfo);
+    logger.info(`User Created: ${JSON.stringify(userInfo)}`);
 });
